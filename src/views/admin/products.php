ee561528dfe92e981c2787b0c037bb5c48e6ef36
Pedro Henrique Pinheiro Silva <92695390+Pedrohenriquepinheiro@users.noreply.github.com>
2023-06-24 02:13:10 -0300

    update

--- src/views/admin/products.php
+++ src/views/admin/products.php
@@ -42,6 +42,7 @@ include_once '../../../assets/html/head.html';
             <button type="button" class="btn btn-warning rounded-pill" data-bs-toggle="modal" data-bs-target="#verticalycentered">
                 Cadastrar <i class="bi bi-plus-circle-fill"></i>
             </button>
+
             <div class="modal fade" id="verticalycentered" tabindex="-1" aria-hidden="true" style="display: none;">
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
@@ -51,6 +52,7 @@ include_once '../../../assets/html/head.html';
                         </div>
                         <form action="../../controllers/ProductsController.php" method="POST">
                             <input type="hidden" name="add" value="true">
+                            <input type="hidden" name="id">
                             <div class="modal-body">
                                 <div class="row">
                                     <div class="col-md-4 form-floating mb-3">
@@ -130,9 +132,9 @@ include_once '../../../assets/html/head.html';
                                         <tr class="table-primary">
                                             <th scope="col">Categoria</th>
                                             <th scope="col">Produto</th>
-                                            <th scope="col">Valor</th>
+                                            <th scope="col">Valor(R$)</th>
                                             <th scope="col">Quantidade (unidades)</th>
-                                            <th scope="col">Data últ. reposição</th>
+                                            <th scope="col">Fonecedor</th>
                                             <th scope="col"></th>
                                         </tr>
                                     </thead>
@@ -143,18 +145,115 @@ include_once '../../../assets/html/head.html';
                                             <tr>
                                                 <td><?= $products['categoria'] ?></td>
                                                 <td><?= $products['produto'] ?></td>
-                                                <td><?= $products['valor_produto'] ?></td>
+                                                <td>R$ <?= $products['valor_produto'] ?></td>
                                                 <td><?= $products['quantidade'] ?></td>
-                                                <td><?= date('d/m/Y', strtotime($products['data'])) ?></td>
-                                                <td></td>
+                                                <td><?= $products['fornecedor'] ?></td>                                               
+                                                <td>
+                                                    <a role="button" data-bs-toggle="modal" data-bs-target="#EditProducts_<?= $products['id'] ?>">
+                                                        <i class="bi bi-pencil-square mr-1" style="color:#343a40;" title="Editar Produto"></i>
+                                                    </a>
+
+                                                    <!-- Modal Edit -->
+                                                    <div class="modal fade" id="EditProducts_<?= $products['id'] ?>">
+                                                        <div class="modal-dialog">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                </div>
+                                                                <form action="../../controllers/ProductsController.php" method="POST">
+                                                                    <input type="hidden" name="edit" value="true">
+                                                                    <input type="hidden" name="id" value="<?= $products['id'] ?>">
+                                                                    <div class="col-md-12 form-floating mb-3">
+                                                                        <select name="category" id="category" class="form-select" required>
+                                                                            <option value="alcoólica" <?php if ($products['categoria'] == 'alcoólica') { ?> selected <?php } ?>>Bebida alcoólica</option>
+                                                                            <option value="não alcoólica" <?php if ($products['categoria'] == 'não alcoólica') { ?> selected <?php } ?>>Bebida não alcoólica</option>
+                                                                            <option value="Porções" <?php if ($products['categoria'] == 'Porções') { ?> selected <?php } ?>>Porções</option>
+                                                                        </select>
+                                                                        <label for="category">
+                                                                            <strong> Categoria </strong>
+                                                                        </label>
+                                                                    </div>
+                                                                    <div class="col-md-12 form-floating mb-3">
+                                                                        <input value="<?= $products['produto'] ?>" type="text" name="product" id="products" class="form-control" placeholder="Produto" required>
+                                                                        <label for="products">
+                                                                            <strong> Produto </strong>
+                                                                        </label>
+                                                                    </div>
+                                                                    <div class="col-md-12 form-floating mb-3">
+                                                                        <input value="<?= $products['valor_produto'] ?>" type="number" name="value" id="value" class="form-control" placeholder="Valor(R$)" required>
+                                                                        <label for="value">
+                                                                            <strong> valor(R$) </strong>
+                                                                        </label>
+                                                                    </div>
+                                                                    <div class="col-md-12 form-floating mb-3">
+                                                                        <input value="<?= $products['quantidade'] ?>" type="number" name="quantity" id="quantity" class="form-control" placeholder="Quantidade" required>
+                                                                        <label for="quantity">
+                                                                            <strong> Quantidade </strong>
+                                                                        </label>
+                                                                    </div>
+                                                                    <div class="col-md-12 form-floating mb-3">
+                                                                        <input value="<?= $products['fornecedor'] ?>" type="text" name="supplier" id="supplier" class="form-control" placeholder="Quantidade" required>
+                                                                        <label for="supplier">
+                                                                            <strong> Fornecedor </strong>
+                                                                        </label>
+                                                                    </div>
+                                                                    <div class="modal-footer">
+                                                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+                                                                            <i class="bi bi-x-square"></i>
+                                                                        </button>
+                                                                        <button type="submit" class="btn btn-success">
+                                                                            <i class="bi bi-check-square"></i>
+                                                                        </button>
+                                                                    </div>
+                                                            </div>
+                                                        </div>
+                                                        </form>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal" title="Não">
+                                                                <i class="bi bi-x-square"></i>
+                                                            </button>
+                                                            <button type="submit" class="btn btn-success" title="Sim">
+                                                                <i class="bi bi-check-square"></i>
+                                                            </button>
+                                                        </div>
+                                                    </div>
+
+                                                    <a role="button" data-bs-toggle="modal" data-bs-target="#DeleteProducts_<?= $products['$id'] ?>">
+                                                        <i style="cursor:pointer" class="bi bi-trash3"></i>
+                                                    </a>
+
+                                                    <!-- Modal Delete -->
+                                                    <div class="modal fade" id="DeleteProducts_<?= $products['$id'] ?>">
+                                                            <div class="modal-dialog ">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        <strong style="font-size: 24px;"> DESEJA EXCLUIR ESSE PRODUTO "<?= $products['produto'] ?>"? </strong>
+                                                                    </div>
+                                                                    <div class="modal-footer">
+                                                                        <form action="../../controllers/ProductsController.php" method="POST">
+                                                                        <input type="hidden" name="delete" value="true">
+                                                                        <input type="hidden" name="id" value="<?= $products['id'] ?>">
+                                                                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal" title="Não">
+                                                                                <i class="bi bi-x-square"></i>
+                                                                            </button>
+                                                                            <button type="submit" class="btn btn-success" title="Sim">
+                                                                                <i class="bi bi-check-square"></i>
+                                                                            </button>
+                                                                        </form>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                </td>
                                             </tr>
                                         <?php } ?>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
-
-
                     </div>
                 </div>
             </div>
@@ -193,6 +292,51 @@ if (isset($_SESSION['register_products_fail'])) {
     </script>
 <?php
     unset($_SESSION['register_products_fail']);
+}
+?>
+
+
+<?php
+if (isset($_SESSION['edit_products_success'])) {
+?>
+    <script>
+        swalEditSuccess();
+    </script>
+<?php
+    unset($_SESSION['edit_products_success']);
+}
+?>
+
+<?php
+if (isset($_SESSION['edit_products_fail'])) {
+?>
+    <script>
+        swalEditFailed();
+    </script>
+<?php
+    unset($_SESSION['edit_products_fail']);
+}
+?>
+
+<?php
+if (isset($_SESSION['delete_products_success'])) {
+?>
+    <script>
+        swalDeleteSuccess();
+    </script>
+<?php
+    unset($_SESSION['delete_products_success']);
+}
+?>
+
+<?php
+if (isset($_SESSION['delete_products_fail'])) {
+?>
+    <script>
+        swalDeleteFailed();
+    </script>
+<?php
+    unset($_SESSION['delete_products_fail']);
 }
 ?>
 
