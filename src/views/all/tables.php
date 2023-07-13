512f51890835799c213bb34644d6b0e2a3e98da6
JoaoPedroSH <joaopedro.shalom27@gmail.com>
2023-07-13 02:53:24 -0300

    update

--- src/views/all/tables.php
+++ src/views/all/tables.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ 
 session_start();
 require_once '../../../config/ConnectionDB.php';
 ?>
